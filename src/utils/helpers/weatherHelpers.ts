2e1ba2ecf67da284660bf0c19d78e95c96ad953a
arcmena <marcelomena010@gmail.com>
2021-11-11 13:23:56 -0300

    chore: disable unused var error

--- src/utils/helpers/weatherHelpers.ts
+++ src/utils/helpers/weatherHelpers.ts
@@ -1,7 +1,8 @@
 import { WEATHER_ICON_SIZE, WEATHER_UNIT } from '../constants/weatherConstants'
 
 export const formatTemperature = (temp: number) => {
-  const [decimal, _fraction] = String(temp).split('.')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [decimal, fraction] = String(temp).split('.')
 
   return `${decimal} ${WEATHER_UNIT.SYMBOL}`
 }
